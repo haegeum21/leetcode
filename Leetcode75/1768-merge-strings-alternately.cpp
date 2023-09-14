19f127109f73a5d906266bdedc873d44f467953f
haegeum21 <omarkul.md@gmail.com>
2023-09-14 09:13:07 +0600

    Leetcode75 challenge && 1768 problem

---
+++ Leetcode75/1768-merge-strings-alternately.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string mergeAlternately(string s, string t) {
+        string ans;
+        for(int i = 0 ; i < s.size() || i < t.size(); i++){
+            if(i < s.size()) ans.push_back(s[i]);
+            if(i < t.size()) ans.push_back(t[i]);
+        }
+        return ans;
+    }
+};
